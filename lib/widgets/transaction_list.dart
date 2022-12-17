ac96a48c465d8b5ad2034ab29c0b345f9b7bf659
AjibsBaba <samuelajibade22@gmail.com>
2022-12-17 15:00:26 +0100

    refactored widgets

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: transactions.map((tx) {
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 10.0),
+                decoration: BoxDecoration(
+                    border: Border.all(width: 0.3, color: Colors.black)),
+                padding: const EdgeInsets.all(10),
+                child: Text('\$${tx.amount}'),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    tx.title,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(DateFormat().format(tx.date))
+                ],
+              )
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
